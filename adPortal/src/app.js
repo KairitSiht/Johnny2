8df1818112ed0f6f04216475162049968e816f17
KairitDaGreat <kairit.siht@outlook.com>
2017-04-08 22:32:55 +0300

    backend ja andmebaas suhtluses, frontend suht ossib

--- adPortal/src/app.js
+++ adPortal/src/app.js
@@ -1,11 +1,12 @@
 export class App {
 configureRouter(config, router) {
     this.router = router;
-    config.title = 'My Aurelia äpp';
+    config.title = 'LeaveIt2Johnny';
 
     config.map([
-      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index', title: "Esileht", nav: true },
-      { route: 'people', name: 'people',  moduleId: 'people/people', title: "Inimesed",  nav: true }
+      { route: ['', 'home'],       name: 'home',       moduleId: 'home/index', title: "SIGN UP", nav: true },
+      { route: 'people', name: 'people',  moduleId: 'people/people', title: "LOG IN",  nav: true },
+    { route: 'users', name: 'users',  moduleId: 'users/users', title: "USERS",  nav: true }
     ]);
   }
 }
