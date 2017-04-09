42777c975b2db8ff8a493b1e883a9532a96bd939
SigritSiht <sigrit.siht@gmail.com>
2017-04-09 13:56:19 +0300

    testimine kontrollib vanust (esimene unittest)

--- SimpleRestApp/src/main/java/ttu/tteh/user/UserService.java
+++ SimpleRestApp/src/main/java/ttu/tteh/user/UserService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+	private static final int MIN_AGE = 0;
+	private static final int MAX_AGE = 150;
 	
 	private UserRepository userRepository;
 
@@ -14,7 +17,9 @@ public class UserService {
 	}
 
 	User addUser(User user) {
-		
+		if(user.age < MIN_AGE || MAX_AGE < user.age){
+			return null;
+		}
 		return userRepository.save(user);
 	}
 
--- SimpleRestApp/src/test/java/ttu/tteh/SimpleRestAppApplicationTests.java
+++ SimpleRestApp/src/test/java/ttu/tteh/SimpleRestAppApplicationTests.java
@@ -1,16 +1,53 @@
 package ttu.tteh;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import junit.framework.Assert;
+import ttu.tteh.user.User;
+import ttu.tteh.user.UserController;
+import ttu.tteh.user.UserRepository;
+import ttu.tteh.user.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SimpleRestAppApplicationTests {
 
-	@Test
+	@Autowired
+	UserController ctrl;
+	
+	User testUser1;
+	User testUser2;
+	
+	@Before
+	// kõik testandmed pigem siia dekl, slp siin kõik meetodid saavad kasutada
 	public void contextLoads() {
+		testUser1 = new User();
+		testUser2 = new User();
+		testUser2.userName = "TU2";
+		testUser1.userName = "TU1";
+		testUser1.age = -2;
+		testUser2.age = 200;
+		testUser1.url = "http://example.com";
+		testUser2.url = "http://example.com";
+		testUser1.bio = "nobio";
+		testUser2.bio = "nobio";
+		testUser1.passWord = "hunter2";
+		testUser2.passWord = "hunter2";
 	}
+	
+	@Test
+	public void testDoesNotAcceptTimeTravellers(){
+		User response = ctrl.addUser(testUser1);
+		assertThat(response).isEqualTo(null);
+	}
+	
+	
 
 }
